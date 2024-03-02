4027e65b12e76bf7c515851b487ee1d122f4566b
Emil <emil.mannapov@mail.ru>
2024-03-02 23:52:28 +0500

    ad jevelry

--- Assets/Scriptes/DropSpawner.cs
+++ Assets/Scriptes/DropSpawner.cs
@@ -2,47 +2,49 @@
 
 public class DropSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _waterPrefab;
+    [SerializeField] private GameObject _diamondPrefab,_goldPrefab,_emeraldPrefab,_jevelryGlitterPrefab;
     [SerializeField] private GameObject _lavaPrefab;
-    [SerializeField] private Transform [] _waterStartPosition, _lavaStartPosition;
-    [SerializeField] private short _fluidsAmount;
-    [SerializeField] private bool _initWater, _initLava;
-    [SerializeField] private Transform _fluidsContainer;
+    [SerializeField] private Transform [] _jewelryStartPosition, _lavaStartPosition;
+    [SerializeField] private short _fluidsAmount,_jevelryAmount,_jevelryGlitterAmount;
+    [SerializeField] private bool _initLava, _initJevelry;
 
     private void Start()
     {
-        if (_initWater)
+        if (_initJevelry)
         {
-            foreach (Transform tempTranform in _waterStartPosition)
+            foreach (Transform tempTranform in _jewelryStartPosition)
             {
-                WaterInstantiate(tempTranform, _fluidsAmount, _waterPrefab);
+                JewelryInstantiate(tempTranform);
             }
         }
         if (_initLava)
         {
             foreach (Transform tempTranform in _lavaStartPosition)
             {
-                LavaInstantiate(tempTranform, _fluidsAmount, _lavaPrefab);
+                LavaInstantiate(tempTranform);
             }
         }
     }
 
-    private void WaterInstantiate(Transform waterPos, short fludsSum,GameObject waterPrefab)
-    {      
-        for (int i = 0; i <= fludsSum; i++)
-        {
-            float random = Random.Range(-0.2f, 0.2f);
-            var water = Instantiate(waterPrefab, _fluidsContainer);
-            water.transform.position = new Vector3(waterPos.position.x + random, waterPos.position.y + random, 0f);        
-        }
+    private void JewelryInstantiate(Transform startPos)
+    {
+        RandInstantiate(startPos, _jevelryAmount, _diamondPrefab);
+        RandInstantiate(startPos, _jevelryAmount, _goldPrefab);
+        RandInstantiate(startPos, _jevelryAmount, _emeraldPrefab);
+        RandInstantiate(startPos, _jevelryGlitterAmount, _jevelryGlitterPrefab);
     }
-    private void LavaInstantiate(Transform lavaPos, short fludsSum, GameObject lavaPrefab)
+    private void LavaInstantiate(Transform startPos)
+    {
+        RandInstantiate(startPos, _fluidsAmount, _lavaPrefab);
+    }
+
+    private void RandInstantiate(Transform Pos, short fludsSum, GameObject Prefab)
     {
         for (int i = 0; i <= fludsSum; i++)
         {
             float random = Random.Range(-0.2f, 0.2f);
-            var lava = Instantiate(lavaPrefab, _fluidsContainer);
-            lava.transform.position = new Vector3(lavaPos.position.x + random, lavaPos.position.y + random, 0f);
+            var temp = Instantiate(Prefab);
+            temp.transform.position = new Vector3(Pos.position.x + random, Pos.position.y + random, 0f);
         }
     }
 }
